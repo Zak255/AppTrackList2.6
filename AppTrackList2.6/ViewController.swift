e003c86b0f2db47e9079a6461390617fd54883d1
Zakir.Kazimov <ramzan666666@gmail.com>
2021-08-22 15:45:12 +0300

    Initial Commit

---
+++ AppTrackList2.6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTrackList2.6
+//
+//  Created by Zakir on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
